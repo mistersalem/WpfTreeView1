a55e7d3f262bb3127ca775d02774a5610edc7b4a
mastersalem <mikolaj.sumyk.art@gmail.com>
2021-05-18 13:23:44 +0200

    reparing images not showing

--- WpfTreeView/MainWindow.xaml.cs
+++ WpfTreeView/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +21,37 @@ namespace WpfTreeView
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
+
+        /// <summary>
+        /// Defalt constructor
+        /// </summary>
+
         public MainWindow()
         {
             InitializeComponent();
-            
+
         }
+
+        #endregion
+
+        #region On Loaded
+
+        /// <summary>
+        /// When the application first opens
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem();
+
+                FolderView.Items.Add(item);
+            }
+        }
+
+        #endregion
     }
 }
